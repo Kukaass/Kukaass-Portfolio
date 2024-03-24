873ea2e9de5b768b4783f3c6657e066ed511731c
Chester Luke Maligaso <chestermaligaso29@gmail.com>
2024-03-24 11:12:05 +0800

    Client Reviews added

--- src/Components/Reviews.tsx
+++ src/Components/Reviews.tsx
@@ -1,6 +1,15 @@
+import ReviewsSlider from "./ReviewsSlider";
+
 const Reviews = () => {
     return(
-        <div>Reviews lang</div>
+        <div className="bg-[#02050a] pt-[4rem] md:pt-[4rem]">
+            <h1 className="heading">  
+                Client <span className="text-yellow-400">Review</span>
+            </h1>
+            <div className="pt-[5rem] pb-[5rem] w-[80%] mx-auto">
+                <ReviewsSlider />
+            </div>
+        </div>
     )
 }
 

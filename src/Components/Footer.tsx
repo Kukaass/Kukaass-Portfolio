78357568a42ed4d76c8446bb27e0f7bb66c3d06f
Chester Luke Maligaso <chestermaligaso29@gmail.com>
2024-03-24 13:36:56 +0800

    Added contact info

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -3,7 +3,7 @@ import { DevicePhoneMobileIcon, EnvelopeIcon, MapIcon } from "@heroicons/react/2
 const Footer = () => {
     return(
         <div className="pt-[8rem] pb-[4rem] bg-[#02050a]" id="contact">
-            <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto gap-[3rem]">
+            <div className="grid border-b-[1px] pb-[6rem] border-gray-400 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-[80%] mx-auto gap-[3rem] cursor-pointer">
                 <div className="flex items-center space-x-6">
                     <div className="md:w-[6.6rem] md:h-[6.5rem] w-[5rem] h-[5rem] flex items-center justify-center rounded-full bg-[#55e6a5]">
                         <MapIcon className="w-[3.4rem] h-[3.5rem] md:h-[4rem] md:w-[4rem] text-black"/>
@@ -12,8 +12,8 @@ const Footer = () => {
                         <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
                             Address
                         </h1>
-                        <p className="text-[18px] w-[90%] text-white opacity-80">
-                            Lorem ipsum dolor
+                        <p className="text-[14px] w-[90%] text-white opacity-80">
+                            Libtangin, Gasan, Marinduque
                         </p>
                     </div>
                 </div>
@@ -25,9 +25,11 @@ const Footer = () => {
                         <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
                             Contact
                         </h1>
-                        <p className="text-[18px] w-[90%] text-white opacity-80">
-                            +639123456789 <br />
-                            +639987654321
+                        <p className="text-[18px] w-[90%] text-white opacity-80 footer-link">
+                            +639701294465
+                        </p>
+                        <p className="text-[18px] w-[90%] text-white opacity-80 footer-link">
+                            +639641783807
                         </p>
                     </div>
                 </div>
@@ -39,21 +41,20 @@ const Footer = () => {
                         <h1 className="text-[25px] mb-[0.2rem] font-semibold text-white">
                             Email
                         </h1>
-                        <p className="text-[18px] w-[90%] text-white opacity-80">
-                            info@example.com <br />
-                            info@example.edu.ph
+                        <p className="text-[18px] w-[90%] text-white opacity-80 footer-link">
+                            Kukas@example.com
                         </p>
                     </div>
                 </div>
             </div>
             <div className="w-[80%] mt-[2rem] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-between">
-                <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-0">
+                <div className="text-[16px] mb-[2rem] md:mb-0 text-white opacity-20 cursor-pointer">
                     Kukaass - Dev 2024 | All Rights Reserved
                 </div>
                 <div className="flex items-center space-x-10">
-                    <p className="text-[16px] text-white opacity-20">Terms & Conditions</p>
-                    <p className="text-[16px] text-white opacity-20">Privacy Policy</p>
-                    <p className="text-[16px] text-white opacity-20">Sitemap</p>
+                    <p className="text-[16px] text-white opacity-20 hover:opacity-45 cursor-pointer">Terms & Conditions</p>
+                    <p className="text-[16px] text-white opacity-20 hover:opacity-45 cursor-pointer">Privacy Policy</p>
+                    <p className="text-[16px] text-white opacity-20 hover:opacity-45 cursor-pointer">Sitemap</p>
                 </div>
             </div>
         </div>
